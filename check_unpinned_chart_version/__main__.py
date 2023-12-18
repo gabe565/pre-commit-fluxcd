5a2dccdec0988979d1e32dec1b5f5ededd22068b
Gabe Cook <gabe565@gmail.com>
2023-12-18 16:29:06 -0600

    chore: Split main logic into functions

--- check_unpinned_chart_version/__main__.py
+++ check_unpinned_chart_version/__main__.py
@@ -5,22 +5,27 @@ from os.path import abspath
 from lib.paths import *
 
 
+def check_helm_release(path):
+    with open(path) as file:
+        try:
+            for doc in yaml.safe_load_all(file):
+                if "kind" not in doc or doc["kind"] != "HelmRelease":
+                    continue
+                if "version" not in doc["spec"]["chart"]["spec"]:
+                    print(f"HelmRelease missing version: {abspath(path)}")
+                    return False
+        except Exception as err:
+            print(f"HelmRelease malformed: {abspath(path)}")
+            print(err)
+            return False
+    return True
+
+
 def main():
-    failed = False
+    success = False
     for path in argv_or_glob(glob_yaml):
-        with open(path) as file:
-            try:
-                for doc in yaml.safe_load_all(file):
-                    if "kind" not in doc or doc["kind"] != "HelmRelease":
-                        continue
-                    if "version" not in doc["spec"]["chart"]["spec"]:
-                        print(f"HelmRelease missing version: {abspath(path)}")
-                        failed = True
-            except Exception as err:
-                print(f"HelmRelease malformed: {abspath(path)}")
-                print(err)
-                failed = True
-    if failed:
+        success = check_helm_release(path) and success
+    if not success:
         exit(1)
 
 
