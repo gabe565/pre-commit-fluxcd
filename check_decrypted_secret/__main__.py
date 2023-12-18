ef2c955419e4694e5e1ec2295ba46a934b007e51
Gabe Cook <gabe565@gmail.com>
2023-12-18 16:29:06 -0600

    chore: Split main logic into functions

--- check_decrypted_secret/__main__.py
+++ check_decrypted_secret/__main__.py
@@ -5,29 +5,38 @@ import yaml
 from os.path import abspath
 from lib.paths import *
 
+env_mac_key = re.compile("^sops_mac=")
+
+
+def check_env(path):
+    with open(path) as file:
+        for line in file:
+            if env_mac_key.search(line):
+                return True
+    print(f"Env file decrypted: {abspath(path)}")
+    return False
+
+
+def check_secret(path):
+    with open(path) as file:
+        for doc in yaml.safe_load_all(file):
+            if "kind" not in doc or doc["kind"] != "Secret":
+                continue
+            if "stringData" not in doc and "data" not in doc:
+                continue
+            if "sops" not in doc:
+                print(f"Secret file decrypted: {abspath(path)}")
+                return False
+    return True
+
 
 def main():
-    failed = False
-    env_mac_key = re.compile("^sops_mac=")
+    success = True
     for path in argv_or_glob(glob_env):
-        with open(path) as file:
-            for line in file:
-                if env_mac_key.search(line):
-                    break
-            else:
-                print(f"Env file decrypted: {abspath(path)}")
-                failed = True
+        success = check_env(path) and success
     for path in argv_or_glob(glob_yaml):
-        with open(path) as file:
-            for doc in yaml.safe_load_all(file):
-                if "kind" not in doc or doc["kind"] != "Secret":
-                    continue
-                if "stringData" not in doc and "data" not in doc:
-                    continue
-                if "sops" not in doc:
-                    print(f"Secret file decrypted: {abspath(path)}")
-                    failed = True
-    if failed:
+        success = check_secret(path) and success
+    if not success:
         exit(1)
 
 
