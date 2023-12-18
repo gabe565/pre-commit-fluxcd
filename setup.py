667cf4121d64646d2091b46c3be3c68d524bb61f
Gabe Cook <gabe565@gmail.com>
2023-12-18 17:41:52 -0600

    chore: Bump version to v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pre-commit-gitops",
-    version="0.0.1",
+    version="0.0.2",
     description="Pre-Commit hooks for GitOps repos",
     long_description=long_description,
     long_description_content_type="text/markdown",
